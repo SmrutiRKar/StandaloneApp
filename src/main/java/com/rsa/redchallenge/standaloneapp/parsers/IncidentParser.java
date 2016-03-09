1d851d9208deee088db5fa7f53f8d166ed2daf91
Akash Anjanappa <akash.anjanappa@emc.com>
2016-03-09 15:23:23 +0530

    relogin if jsession is expired

--- src/main/java/com/rsa/redchallenge/standaloneapp/parsers/IncidentParser.java
+++ src/main/java/com/rsa/redchallenge/standaloneapp/parsers/IncidentParser.java
@@ -6,6 +6,8 @@ import com.rsa.redchallenge.standaloneapp.utils.RestInteractor;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.codehaus.jettison.json.JSONObject;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -16,6 +18,7 @@ import java.util.Map;
  */
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class IncidentParser {
 
     private static final Log log = LogFactory.getLog(IncidentParser.class);
@@ -24,13 +27,9 @@ public class IncidentParser {
         String result = "";
         RestInteractor restInteractor =  new RestInteractor();
         result =  restInteractor.performGet(String.class, ApplicationConstant.SA_BASE_URL + ApplicationConstant.INCIDENT_DASHBOARD_URI,null,sessionId);
-        if(checkIfJsessionIdExpired(result)){
-            //re login to get the new jessionId...
-            log.info("old expired jsession id is:"+sessionId);
-            SecurityParser.performReLoginAndUpdateJsessionId(ApplicationConstant.sessionIdMapByApplicationUser.get(user));
-            sessionId = ApplicationConstant.sessionIdMapByApplicationUser.get(user).getSaSessionId();
-            log.info("new jsession id is:"+sessionId +" performing the request again to fetch data..");
-            result =  restInteractor.performGet(String.class, ApplicationConstant.SA_BASE_URL + ApplicationConstant.INCIDENT_DASHBOARD_URI,null,sessionId);
+        if(checkIfJsessionIdExpired(result,user)){
+            result =  restInteractor.performGet(String.class, ApplicationConstant.SA_BASE_URL + ApplicationConstant.INCIDENT_DASHBOARD_URI,null,
+                    ApplicationConstant.sessionIdMapByApplicationUser.get(user).getSaSessionId());
         }
         return result;
     }
@@ -41,7 +40,10 @@ public class IncidentParser {
         Map<String, Object> paramsMap = new HashMap<String, Object>();
         paramsMap.put("requestParams", requestParams);
         result =  restInteractor.performGet(String.class, ApplicationConstant.SA_BASE_URL + ApplicationConstant.INCIDENT_SUMMARY_URI,paramsMap,sessionId);
-
+        if(checkIfJsessionIdExpired(result,user)){
+            result =  restInteractor.performGet(String.class, ApplicationConstant.SA_BASE_URL + ApplicationConstant.INCIDENT_SUMMARY_URI,paramsMap,
+                    ApplicationConstant.sessionIdMapByApplicationUser.get(user).getSaSessionId());
+        }
         return result;
     }
 
@@ -51,6 +53,10 @@ public class IncidentParser {
         Map<String, Object> paramsMap = new HashMap<String, Object>();
         paramsMap.put("requestPayload", payload);
         result =  restInteractor.performGet(String.class, ApplicationConstant.SA_BASE_URL + ApplicationConstant.EDIT_INCIDENT_URI,paramsMap,sessionId);
+        if(checkIfJsessionIdExpired(result,user)){
+            result =  restInteractor.performGet(String.class, ApplicationConstant.SA_BASE_URL + ApplicationConstant.EDIT_INCIDENT_URI,paramsMap,
+                    ApplicationConstant.sessionIdMapByApplicationUser.get(user).getSaSessionId());
+        }
         return result;
     }
 
@@ -60,12 +66,18 @@ public class IncidentParser {
         Map<String, Object> paramsMap = new HashMap<String, Object>();
         paramsMap.put("incidentId", incidentId);
         result =  restInteractor.performGet(String.class, ApplicationConstant.SA_BASE_URL + ApplicationConstant.INCIDENT_BY_ID_URI,paramsMap,sessionId);
+        if(checkIfJsessionIdExpired(result,user)){
+            result =  restInteractor.performGet(String.class, ApplicationConstant.SA_BASE_URL + ApplicationConstant.INCIDENT_BY_ID_URI,paramsMap,
+                    ApplicationConstant.sessionIdMapByApplicationUser.get(user).getSaSessionId());
+        }
         return result;
     }
 
-    public boolean checkIfJsessionIdExpired(String result){
+    public boolean checkIfJsessionIdExpired(String result,String user) throws  Exception{
         if(result!=null && result!= "" && result.contains("UAP.login.Application")){
             log.error("Jsession Id expired...should login to continue...");
+            SecurityParser.performReLoginAndUpdateJsessionId(ApplicationConstant.sessionIdMapByApplicationUser.get(user));
+            log.info("new jsession id is:"+ApplicationConstant.sessionIdMapByApplicationUser.get(user).getSaSessionId() +" performing the request again to fetch data..");
             return true;
         }
         return false;
